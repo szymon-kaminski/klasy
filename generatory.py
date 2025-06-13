1574220652799c100a32b6376e9f38e36d13237a
Szymon Kaminski <szymon.kaminski46@gmail.com>
2025-06-13 14:50:21 +0200

    Dodanie kolejnego przykładu generatora

--- generatory.py
+++ generatory.py
@@ -4,3 +4,14 @@ def licz_od_1_do(n):
 
 for liczba in licz_od_1_do(5):
     print(liczba)
+
+# Przykład 2:
+print()
+def parzyste_do(n):
+    for i in range(n):
+        if i % 2 == 0:
+            yield i
+
+gen = parzyste_do(10)
+for x in gen:
+    print(x)
