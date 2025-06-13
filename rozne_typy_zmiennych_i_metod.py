433a506b1fa5bfb7fbd7e7244b8272d6816ca3d7
Szymon Kaminski <szymon.kaminski46@gmail.com>
2025-06-13 14:36:33 +0200

    Dodano klasę Produkt z metodami klasowymi, instancyjnymi i statyczną

--- rozne_typy_zmiennych_i_metod.py
+++ rozne_typy_zmiennych_i_metod.py
@@ -26,3 +26,35 @@ Pracownik.zmien_firme("Microsoft")
 p2.przedstaw_sie()
 
 Pracownik.przywitaj()
+
+####################################################
+####################################################
+class Produkt:
+    vat = 0.23
+
+    def __init__(self, nazwa, cena_netto):
+        self.nazwa = nazwa
+        self.cena_netto = cena_netto
+
+    def cena_brutto(self):
+        return self.cena_netto * (1 + Produkt.vat)
+
+    @classmethod
+    def zmien_vat(cls, nowy_vat):
+        if cls.czy_liczba_dodatnia(nowy_vat):
+            cls.vat = nowy_vat
+        else:
+            print("Vat musi być liczbą dodatnią!")
+
+    @staticmethod
+    def czy_liczba_dodatnia(x):
+        return isinstance(x, (int, float)) and x > 0
+
+# Przykład użycia:    
+produkt1 = Produkt("Mleko", 4.00)
+print(f"Brutto: {produkt1.cena_brutto():.2f} zł")
+
+Produkt.zmien_vat(0.1)  # zmieniamy VAT na 10%
+print(f"Nowy brutto: {produkt1.cena_brutto():.2f} zł")
+
+Produkt.zmien_vat(-0.5)  # próba ustawienia niepoprawnego VAT
