761cd41e3ad5bf0d3aabec449df69f2df2621585
Szymon Kaminski <szymon.kaminski46@gmail.com>
2025-06-16 13:52:06 +0200

    Dodanie przykładu na dodawanie z użyciem dekoratorów.

--- dekoratory.py
+++ dekoratory.py
@@ -33,3 +33,21 @@ def licz_do_miliona():
     return suma
 
 licz_do_miliona()
+
+##################################################
+print()
+def dodawanie(funkcja):
+    def opakowanie(*args, **kwargs):
+        print("start")
+        wynik = funkcja(*args, **kwargs)
+        print("Wynik:", wynik)
+        print("koniec")
+        return wynik
+    return opakowanie
+
+@dodawanie
+def dodaj(a, b):
+    return a + b
+
+wynik = dodaj(7, 12)
+
