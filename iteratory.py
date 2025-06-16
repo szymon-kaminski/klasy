28e94e467bd3b643386eac5e7d7db9280ce8c64f
Szymon Kaminski <szymon.kaminski46@gmail.com>
2025-06-16 12:16:48 +0200

    Dodanie przykładu na drukowanie liczb parzystych z użyciem iteratorów

--- iteratory.py
+++ iteratory.py
@@ -8,11 +8,35 @@ class Licznik:
     
     def __next__(self):
         if self.aktualna <= self.n:
-            wartość = self.aktualna
+            wartosc = self.aktualna
             self.aktualna += 1
-            return wartość
+            return wartosc
         else:
             raise StopIteration
 
 for liczba in Licznik(5):
+    print(liczba)
+
+########################################################
+########################################################
+print()
+print("Parzyste do N")
+
+class ParzysteDoN:
+    def __init__(self, n):
+        self.n = n
+        self.aktualna = 0
+
+    def __iter__(self):
+        return self
+    
+    def __next__(self):
+        if self.aktualna <= self.n:
+            wartosc = self.aktualna
+            self.aktualna += 2
+            return wartosc
+        else:
+            raise StopIteration
+        
+for liczba in ParzysteDoN(10):
     print(liczba)
